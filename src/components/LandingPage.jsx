ecacbc1e8012c01eaa0d1a2770f7fb2c05a7a00f
Zacck <coderv63@gmail.com>
2017-02-28 15:11:21 +0200

    work on styling for home page and mark up

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,11 +3,14 @@ import React, {Component} from 'react'
 export class LandingPage extends Component {
   render() {
     return(
-      <div>
-        <div></div>
+      <div className="l-page">
         <div className="text-center">
-          <p>{`"I live my life with stories that came before me.`}</p>
-          <p>{`I tell stories becade of the stories that were told before"`}</p>
+          <img className="color-logo" src={require('../assets/color_logo.png')}></img>
+        </div>
+        <div className="text-center">
+          <div className="carla-quote">{`"I live my life with stories that came before me.`}</div>
+          <div className="carla-quote">{`I tell stories becade of the stories that were told before"`}</div>
+          <p className="quote-name">Carla Inez Espost</p>
         </div>
       </div>
     );
