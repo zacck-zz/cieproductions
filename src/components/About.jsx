cfeb96a74d48dc5a3545d23d26ffadabe9246501
Zacck <coderv63@gmail.com>
2017-04-04 13:49:46 +0200

    work on adding styles for about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,30 @@ import React,{Component} from 'react'
 
 class About extends Component {
   render() {
-    return(<p>About us Page</p>)
+    return(
+      <div className="about-page">
+        <div className="top-section">
+          <p className="text-center we-are">WE ARE VISUAL STORIES</p>
+        </div>
+        <div className="pimages text-center">
+          <div className="blkbg columns large-3 medium -3 small-6">
+            <img className="profile-pic" src={require('../assets/me.png')}></img>
+          </div>
+          <div className="blkbg columns large-3 medium -3 small-6">
+            <img className="profile-pic" src={require('../assets/chexz.png')}></img>
+          </div>
+          <div className="blkbg columns large-3 medium -3 small-6">
+            <img className="profile-pic" src={require('../assets/aphelele.png')}></img>
+          </div>
+          <div className="blkbg columns large-3 medium -3 small-6">
+            <img className="profile-pic" src={require('../assets/maishe.png')}></img>
+          </div>
+        </div>
+        <div className="blkbg">
+
+        </div>
+      </div>
+    )
   };
 };
 
