b9280debd527ea1d76eb5a7c34acfbb2ae5cb764
Zacck <coderv63@gmail.com>
2017-04-13 09:41:42 +0200

    work on landing page styling

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,10 +6,10 @@ export class LandingPage extends Component {
         <div className="text-center">
           <img className="color-logo" src={require('../assets/color_logo.png')}></img>
         </div>
-        <div className="text-center">
+        <div className="text-center quote-box">
           <div className="carla-quote">{`"I live my life with stories that came before me.`}</div>
-          <div className="carla-quote">{`I tell stories becade of the stories that were told before"`}</div>
-          <p className="quote-name">Carla Inez Espost</p>
+          <div className="carla-quote">{`I tell stories because of the stories that were told before"`}</div>
+          <p className="quote-name">- Carla Inez Espost</p>
         </div>
       </div>
     );
