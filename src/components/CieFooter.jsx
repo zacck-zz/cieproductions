4fbc91148ccfc9be782fcdd97aed1e5b41a50005
Zacck <coderv63@gmail.com>
2017-05-08 15:03:34 +0200

    work on style

--- src/components/CieFooter.jsx
+++ src/components/CieFooter.jsx
@@ -7,22 +7,22 @@ export class CieFooter extends Component {
         <div className="foot text-center">
           <img className="about-logo" src={require('../assets/medium_footer_logo.png')}></img>
           <div className="social-box">
-            <div className="columns small-2 social-child">
+            <div className="social-child">
               <img className="social-logo" src={require('../assets/social/linkedin.png')}></img>
             </div>
-            <div  className="columns small-2 social-child">
+            <div  className="social-child">
               <img className="social-logo" src={require('../assets/social/facebook.png')}></img>
             </div>
-            <div  className="columns small-2 social-child">
+            <div  className="social-child">
               <img className="social-logo" src={require('../assets/social/vimeo.png')}></img>
             </div>
-            <div  className="columns small-2 social-child">
+            <div  className="social-child">
               <img className="social-logo" src={require('../assets/social/twitter.png')}></img>
             </div>
-            <div  className="columns small-2 social-child">
+            <div  className="social-child">
               <img className="social-logo" src={require('../assets/social/youtube.png')}></img>
             </div>
-            <div  className="columns small-2 social-child">
+            <div  className="social-child">
               <img className="social-logo" src={require('../assets/social/instagram.png')}></img>
             </div>
           </div>
